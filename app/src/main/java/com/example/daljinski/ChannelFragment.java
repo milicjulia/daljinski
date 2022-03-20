3e830d5a2af72d93391ffaa1558e8de39d767adf
vnesic <vladimirs.nesic@gmail.com>
2022-03-20 12:08:19 +0100

    Ispravka za odabir kanala iz Channel Fragmenta

--- app/src/main/java/com/example/daljinski/ChannelFragment.java
+++ app/src/main/java/com/example/daljinski/ChannelFragment.java
@@ -13,10 +13,27 @@ public class ChannelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
+
         view= inflater.inflate(R.layout.fragment_channel, container, false);
 
-
-
+        view.findViewById(R.id.svikanali2).findViewById(R.id.gledaj).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MeniFragment.setChannel(2500);
+            }
+        });
+        view.findViewById(R.id.svikanali1).findViewById(R.id.gledaj).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MeniFragment.setChannel(1500);
+            }
+        });
+        view.findViewById(R.id.svikanali).findViewById(R.id.gledaj).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MeniFragment.setChannel(3500);
+            }
+        });
         return view;
     }
 }
